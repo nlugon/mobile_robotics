6b14d0a6b7489e65b1cff9135032cd21a6602d64
Heatdh <rayendhahri@gmail.com>
2022-12-01 00:36:19 +0100

    added movements

---
+++ obstacle/movements.py
@@ -0,0 +1,86 @@
+import time 
+import math
+from tdmclient import ClientAsync, aw
+import asyncio
+import json
+
+
+class Movement:
+
+    def __init__(self, robot):
+        self.robot_ctr = robot
+        self.forward_speed = 100 # just  to test
+        self.right_wheel_offset = 0
+        self.left_wheel_offset = 0
+        self.rotation_speed = 100
+        self.rot_delta_t= 10
+
+    async def motors(self,left, right):
+        return {
+            "motor.left.target": [left],
+            "motor.right.target": [right],
+        }
+    async def stop(self):
+        await self.robot_ctr.set_variables({"motor.left.target": [0]})
+        await self.robot_ctr.set_variables({"motor.right.target": [0]})
+
+    async def step_forward(self,forward_t):
+        await self.robot_ctr.set_variables({"motor.left.target": [self.forward_speed + self.left_wheel_offset]})
+        await self.robot_ctr.set_variables({"motor.right.target": [self.forward_speed + self.right_wheel_offset]})
+        time.sleep(forward_t)
+        await self.stop()
+
+    async def turn_left(self,diff_orient):
+        #90 degree turn
+        await self.robot_ctr.set_variables({"motor.left.target": [2**16-self.self.rotation_speed]})
+        await self.robot_ctr.set_variables({"motor.right.target": [self.rotation_speed]})
+        time.sleep(diff_orient*self.rot_delta_t)
+        await self.stop()
+
+    async def turn_right(self,diff_orient):
+        #90 degree turn
+        await self.robot_ctr.set_variables({"motor.left.target": [self.rotation_speed]})
+        await self.robot_ctr.set_variables({"motor.right.target": [2**16-self.rotation_speed]})
+        time.sleep(2.33) 
+        await self.stop()
+    
+
+    def turn(self,ang):
+        if ang > 0:
+            self.turn_right(ang)
+        else:
+            self.turn_left(-ang)
+
+    
+
+        
+    def regulateAng(self,ang):
+        if ang > math.pi:
+            return ang - 2 * math.pi
+        elif ang < -math.pi:
+            return ang + 2 * math.pi
+        else:
+            return ang
+
+async def main():
+    client = ClientAsync()
+    node = await client.lock()
+    await node.wait_for_variables()
+    move_obj = Movement(node)
+    #node.send_set_variables(move_obj.motors(100,100))
+    #time.sleep(5)
+    #node.send_set_variables(move_obj.motors(0,0))
+    #time.sleep(2)
+    #await move_obj.turn_right(10)
+    #time.sleep(2)
+    #await move_obj.stop()
+    await move_obj.step_forward(3)
+    await move_obj.turn_right(3)
+    print("Variables loaded", node.var)
+    
+
+# execute the main function
+if __name__ == "__main__":
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main())
+    loop.close()
---
+++ obstacle/objavoidance.py
@@ -0,0 +1,82 @@
+import tdmclient
+from tdmclient import ClientAsync, aw
+import time
+import math
+import asyncio
+def motors(l_speed=500, r_speed=500, verbose=False):
+    return {
+        "motor.left.target": [l_speed],
+        "motor.right.target": [r_speed],
+    }
+
+
+
+def object_close(node,wall_threshold, verbose=False):
+    if any([x>wall_threshold for x in node['prox.horizontal'][:-2]]):
+        return True
+    return False
+
+async def follow_obj(node,client,motor_speed=70, wall_threshold=1000, white_threshold=200, verbose=False):
+
+    await node.set_variables(motors(motor_speed, motor_speed))
+           
+    prev_state="forward"
+    goal = False
+    # number of obstacle avoided
+    num_obstacle = 0
+    while not goal:
+        
+        if object_close(node,wall_threshold):
+            if prev_state=="forward": 
+                await node.set_variables(motors(motor_speed, -motor_speed))
+                prev_state="turning"
+                num_obstacle += 1
+                print("Obstacle detected, turning")
+        if num_obstacle > 200:
+            goal = True
+            print("Goal reached")
+            await node.set_variables(motors(0, 0))
+            break 
+        else:
+            if prev_state=="turning": 
+                await node.set_variables(motors(motor_speed, motor_speed))
+                prev_state="forward"
+
+        await client.sleep(0.1) 
+        # exit after 200 obstacles
+      
+
+    return 
+
+
+
+async def main():
+    client = ClientAsync()
+    node = await client.lock()
+    await node.wait_for_variables()
+    await follow_obj(node,client)
+    await client.sleep(10)
+    await node.set_variables(motors(0, 0))
+
+    # if close then set motors to 0,0
+
+
+
+    #node.send_set_variables(move_obj.motors(100,100))
+    #time.sleep(5)
+    #node.send_set_variables(move_obj.motors(0,0))
+    #time.sleep(2)
+    #await move_obj.turn_right(10)
+    #time.sleep(2)
+    #await move_obj.stop()
+    #print("Variables loaded", node.var)
+    
+
+# execute the main function
+if __name__ == "__main__":
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main())
+    loop.close()
+
+
+ 
