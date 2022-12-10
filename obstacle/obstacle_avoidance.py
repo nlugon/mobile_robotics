1eef8d7b73ea4e97382211c8a8116e2ebb302a44
Heatdh <rayendhahri@gmail.com>
2022-12-10 11:38:16 +0100

    add kidnapp wrapper

--- obstacle/obstacle_avoidance.py
+++ obstacle/obstacle_avoidance.py
@@ -21,42 +21,56 @@ def led(color):
 
 def close_by(node, variables):
     try:
-        
-        if any([x>21000 and x < 31000 for x in variables["prox.horizontal"]]):
-            print("obstacle is very close") 
-            avoid = False
-            #prox = variables["prox.horizontal"]
-            var_prox= np.array(variables["prox.horizontal"])
-            if (var_prox[:1].sum() > var_prox[3:4].sum()) and (var_prox[2] < 3000):
-                node.send_set_variables(move(100, -100))
-            elif var_prox[2] > 3000: 
-                node.send_set_variables(move(100, -100))
-
+        # check for kidnapping 
+        thymio_kidnapped = False
+        if any([x< 20 for x in variables["prox.ground.delta"]]):
+            print("thymio is kidnapped")
+            node.send_set_variables(move(0, 0))
+            thymio_kidnapped = True
+        else:
+            # check if thymio was kidnapped and is now free
+            if thymio_kidnapped:
+                print("thymio is free")
+                node.send_set_variables(move(100, 100))
+                thymio_kidnapped = False
             else:
-                node.send_set_variables(move(-100, 100))
 
-        elif any([x>3700 for x in variables["prox.horizontal"]]):
-            print("obstacle is close")
-            # ON PLACE ROTATION
-            avoid = False
-            #prox = variables["prox.horizontal"]
-            var_prox= np.array(variables["prox.horizontal"])
-            if (var_prox[:1].sum() > var_prox[3:4].sum()):
-                node.send_set_variables(move(0, -100))
-            elif var_prox[2] > 3600:
-                node.send_set_variables(move(0, -100))
-            else:
-                node.send_set_variables(move(-100, 0))
 
-        else :
-            # follow path
-            avoid = True
-            # if position is not in the middle of the path
-            
-            # call get position function and calculate the error
-            # if error is too big, turn
-            print("no obstacle")
-            node.send_set_variables(move(100, 100))
+                if any([x>21000 and x < 31000 for x in variables["prox.horizontal"]]):
+                    print("obstacle is very close") 
+                    avoid = False
+                    #prox = variables["prox.horizontal"]
+                    var_prox= np.array(variables["prox.horizontal"])
+                    if (var_prox[:1].sum() > var_prox[3:4].sum()) and (var_prox[2] < 3000):
+                        node.send_set_variables(move(100, -100))
+                    elif var_prox[2] > 3000: 
+                        node.send_set_variables(move(100, -100))
+
+                    else:
+                        node.send_set_variables(move(-100, 100))
+
+                elif any([x>3700 for x in variables["prox.horizontal"]]):
+                    print("obstacle is close")
+                    # ON PLACE ROTATION
+                    avoid = False
+                    #prox = variables["prox.horizontal"]
+                    var_prox= np.array(variables["prox.horizontal"])
+                    if (var_prox[:1].sum() > var_prox[3:4].sum()):
+                        node.send_set_variables(move(0, -100))
+                    elif var_prox[2] > 3600:
+                        node.send_set_variables(move(0, -100))
+                    else:
+                        node.send_set_variables(move(-100, 0))
+
+                else :
+                    # follow path
+                    avoid = True
+                    # if position is not in the middle of the path
+                    
+                    # call get position function and calculate the error
+                    # if error is too big, turn
+                    print("no obstacle")
+                    node.send_set_variables(move(100, 100))
             
 
 
